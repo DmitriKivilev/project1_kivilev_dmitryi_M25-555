6b9279163b32d388788b77a115f987c4f9a82d60
dmitrikivilev <dimon_95.08@mail.ru>
2025-11-11 18:34:46 +0300

    Add game: constants, player actions, utils

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
+
+from labyrinth_game.constants import ROOMS
+
+# Состояние игры
+game_state = {
+    'player_inventory': [],  # Инвентарь игрока
+    'current_room': 'entrance',  # Текущая комната
+    'game_over': False,  # Флаг окончания игры
+    'steps_taken': 0  # Количество шагов
+}
+
 def main():
     print("Первая попытка запустить проект!")
+    print(f"Текущая комната: {game_state['current_room']}")
+    print(f"Всего комнат в лабиринте: {len(ROOMS)}")
 
 if __name__ == "__main__":
     main()
